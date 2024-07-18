a962088953ea4fb2cf23a83e4140e24aae5816c9
franklin gaspar <frgaspar@apoioprodesp.sp.gov.br>
2024-07-18 14:25:39 -0300

    initial commit

---
+++ app/src/main/java/br/gov/sp/sgf/appimpressora/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.gov.sp.sgf.appimpressora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
